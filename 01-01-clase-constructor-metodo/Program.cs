eef92fa85b8d5b8f52efcab7bc3022fb7b534b5a
JAGuzmanCruzAppDB <ingsistemasjosealfonso@gmail.com>
2023-01-13 23:42:01 -0600

    Recibir 2 valores en el contructor y asignarlos a dos variables locales de la clase 'Operaciones', usando metodos visualizar un resultado en la clase principal.

--- 01-01-clase-constructor-metodo/Program.cs
+++ 01-01-clase-constructor-metodo/Program.cs
@@ -10,6 +10,19 @@ namespace _01_01_clase_constructor_metodo
     {
         static void Main(string[] args)
         {
+            double x = 0, y = 0;
+
+            Console.WriteLine("Ingresa el primer valo: ");
+            x = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Ingresa el segundo valo: ");
+            y = Convert.ToDouble(Console.ReadLine());
+
+            Operaciones op = new Operaciones(x, y);
+
+            op.Suma();
+            op.Visualizar();
+
+            Console.ReadLine();
         }
     }
 }
