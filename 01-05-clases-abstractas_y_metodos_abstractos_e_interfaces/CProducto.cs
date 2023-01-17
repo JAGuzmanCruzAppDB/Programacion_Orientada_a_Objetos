e8f77086e608a7a3ffcf92810f8880322abf1d05
JAGuzmanCruzAppDB <ingsistemasjosealfonso@gmail.com>
2023-01-16 19:03:16 -0600

    la vista es la informacion que es permitida esar para el programador que hace uso de la clase padre

---
+++ 01-05-clases-abstractas_y_metodos_abstractos_e_interfaces/CProducto.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_05_clases_abstractas_y_metodos_abstractos_e_interfaces
+{
+    abstract class CProducto
+    {
+        protected string descripcion;
+        protected double precioCompra;
+        protected double precioVenta;
+
+        public CProducto(string descripcion, double precioCompra)
+        {
+            this.descripcion = descripcion;
+            this.precioCompra = precioCompra;
+            precioVenta = 0.0;
+        }
+        public virtual void CalculaPrecio()
+        {
+            Console.WriteLine("Calcula precio de producto");
+            precioVenta = precioCompra * 1.3;
+        }
+        public abstract void MuestraVenta();
+    }
+}
