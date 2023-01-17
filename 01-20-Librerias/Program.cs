0a8de008c3912199b8f2422f65ef497b415867b4
JAGuzmanCruzAppDB <ingsistemasjosealfonso@gmail.com>
2023-01-16 21:49:37 -0600

    ~

---
+++ 01-20-Librerias/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Aritmetica;
+
+namespace _01_20_Librerias
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double r = 0;
+
+            r = Class1.Suma(2.3, 4.6);
+            Console.WriteLine(r);
+            Console.ReadKey();
+        }
+    }
+}
