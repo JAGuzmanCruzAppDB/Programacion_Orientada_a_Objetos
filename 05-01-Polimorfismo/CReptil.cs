d30f89d59f615b801acd58e34e1138fae99b45a9
JAGuzmanCruzAppDB <ingsistemasjosealfonso@gmail.com>
2023-01-17 16:24:19 -0600

    con el polimorfismo, el codigo se concentra en el concepto no en la clase

---
+++ 05-01-Polimorfismo/CReptil.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05_01_Polimorfismo
+{
+    class CReptil:CAnimal
+    {
+        private string nombre;
+        public string Nombre { get { return nombre; } set { nombre = value; } }
+        public void Moverse()
+        {
+            Console.WriteLine("El reptil {0} se arrastra", nombre);
+        }
+    }
+}
