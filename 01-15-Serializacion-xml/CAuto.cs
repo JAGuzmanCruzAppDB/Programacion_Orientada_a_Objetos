8818ad1dbebbc18686ace7a5baa29d1f82b9e837
JAGuzmanCruzAppDB <ingsistemasjosealfonso@gmail.com>
2023-01-16 21:29:10 -0600

    ~

---
+++ 01-15-Serializacion-xml/CAuto.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_15_Serializacion_xml
+{
+    [Serializable]
+    public class CAuto
+    {
+        private double costo;
+        private string modelo;
+
+        public double Costo { set { costo = value; } get { return costo; } }
+        public string Modelo { set { modelo = value; } get { return modelo; } }
+
+        //constructor si parametros si se ocupa serializacion de xml
+        public CAuto()
+        {
+            costo = 100000;
+            modelo = "Chevy";
+        }
+
+        public void MuestraInformacion()
+        {
+            //mostramos informacion necesaria
+            Console.WriteLine("Tu Auto {0}", modelo);
+            Console.WriteLine("Costo {0}", costo);
+            Console.WriteLine("----------------------");
+        }
+    }
+}
