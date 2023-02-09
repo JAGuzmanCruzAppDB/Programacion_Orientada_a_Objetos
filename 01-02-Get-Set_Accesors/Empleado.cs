5e13a667cb6c5842087d9808cee61c5866e2b8f0
JAGuzmanCruzAppDB <ingsistemasjosealfonso@gmail.com>
2023-02-09 04:16:09 -0600

    get, set y mutator

---
+++ 01-02-Get-Set_Accesors/Empleado.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_02_Get_Set_Accesors
+{
+    class Empleado
+    {
+        private double sueldo;
+        private double impuesto;
+        private string impuestodel;
+
+        public void set_sueldo(double psueldo)
+        {
+            sueldo = psueldo;
+        }
+        public double get_sueldo()
+        {
+            return sueldo;
+        }
+        public string get_impuesto()
+        {
+            impuestodel = "16%";
+            return impuestodel;
+        }
+        private void mutator()
+        {
+            impuesto = sueldo * 0.16;
+        }
+        public void muestra()
+        {
+            mutator();
+            Console.WriteLine("Empleado 0001 con un sueldo neto de={0}, tendria un impuesto de={1}", sueldo, impuesto);
+        }
+    }
+}
---
+++ 01-02-Get-Set_Accesors/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_02_Get_Set_Accesors
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Empleado em = new Empleado();
+            Console.WriteLine("El impuesto tomado en cuenta es de " + em.get_impuesto());
+            em.set_sueldo(10500.50);
+
+            em.muestra();
+
+            Console.ReadKey();
+        }
+    }
+}
