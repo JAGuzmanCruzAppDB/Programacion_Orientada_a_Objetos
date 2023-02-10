e0441ff5ab413d218dc9cef28fb38f604d1decf5
JAGuzmanCruzAppDB <ingsistemasjosealfonso@gmail.com>
2023-02-10 02:44:16 -0600

    Propiedades de c#

---
+++ 01-02-Propiedades-Get_Set/Empleado.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_02_Propiedades_Get_Set
+{
+    class Empleado
+    {
+        private double sueldo;
+        private double impuesto;
+        private double impuestode;
+
+        public void Muestra()
+        {
+            Console.WriteLine("Sueldo={0}, impuesto={1}", sueldo, impuesto);
+        }
+        public double Sueldo
+        {
+            get { return sueldo; }
+            set { 
+                sueldo = value;
+
+                //if (sueldo > 5000)
+                //{
+                //    sueldo = value;
+                //}
+                //else
+                //{
+                //    sueldo = 3000;
+                //}
+
+            }
+        }
+        public double Impuesto
+        {
+            get { return impuesto; }
+        }
+        public void Mutator(double pimpuesto)
+        {
+            impuestode = pimpuesto/100;
+            impuesto = sueldo * impuestode;
+        }
+    }
+}
---
+++ 01-02-Propiedades-Get_Set/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_02_Propiedades_Get_Set
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double sueldo=15000;
+            double impuestode=16 ;
+            Empleado op = new Empleado();
+            op.Sueldo=sueldo;
+            op.Mutator(impuestode);
+
+            Console.WriteLine("El impuesto del empleado 10000A1 es {0}", op.Impuesto);
+
+            op.Muestra();
+
+            Console.ReadKey();
+        }
+    }
+}
